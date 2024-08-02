bcf04f141d7c13bc7099b4c0a51da32ebce2de8c
ValuCZ <willrosenbergr@gmail.com>
2024-08-02 13:39:36 +0200

    final v 1.0

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-document.getElementById('copy-btn').addEventListener('click', function() {
-    const code = document.getElementById('curl-code').innerText;
-    navigator.clipboard.writeText(code).then(function() {
-        
-    }).catch(function(error) {
-        console.error('Error copying text: ', error);
+document.addEventListener('DOMContentLoaded', function() {
+    const copyButtons = document.querySelectorAll('.copy-btn');
+
+    copyButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const code = button.previousElementSibling.innerText;
+            navigator.clipboard.writeText(code).then(function() {
+                alert('Code copied to clipboard');
+            }).catch(function(error) {
+                console.error('Error copying text: ', error);
+            });
+        });
     });
 });
